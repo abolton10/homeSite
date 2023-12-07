e49137c5e65c082698f82ca46fc27f22205d71db
abolton10 <147441705+abolton10@users.noreply.github.com>
2023-12-07 12:12:15 -0500

    Create db.php

---
+++ db.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "codd.cs.gsu.edu";
+$username = "abolton10";
+$password = "abolton10";
+$dbname = "abolton10";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
